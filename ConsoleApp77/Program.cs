b8eae3954c71bbe29bb65b16e8f314258dd7277f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:31:28 +0330

    Add files via upload

---
+++ ConsoleApp77/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretword = "giraffe";
+            string guess = "";
+            int guesscount = 0;
+            int guesslimit = 3;
+            bool outofGuesses = false;
+
+            while (guess != secretword && !outofGuesses)
+            {
+                if (guesscount < guesslimit)
+                {
+                    Console.Write("Enter guess:");
+
+                    guess = Console.ReadLine();
+                    guess = Console.ReadLine();
+                    guesscount++;
+                }
+                else
+                {
+                    outofGuesses = true;
+                }
+            }
+            if (outofGuesses)
+            {
+                Console.Write("you lost !");
+            }
+            else
+            {
+                Console.Write("you win!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
